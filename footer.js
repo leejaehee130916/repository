4e547c35eec227515ac2d3fcd13a623d16316b20
leejaehee130916 <david130916@goedu.kr>
2026-07-28 15:01:30 +0900

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const footerHTML = `
+    <footer>
+      <p>© 2026 이재희 체스 프로젝트 | 오늘도 체스 공부 어떠세요?</p>
+    </footer>
+  `;
+  document.body.insertAdjacentHTML("beforeend", footerHTML);
+});
