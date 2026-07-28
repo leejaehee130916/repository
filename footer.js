252adedefd3db5d193561ecf518aef952f16e154
leejaehee130916 <david130916@goedu.kr>
2026-07-28 15:00:55 +0900

    Delete footer.js

--- footer.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const footerHTML = `
-    <footer>
-      <p>© 2026 이재희 체스 프로젝트 | 오늘도 체스 공부 어떠세요?</p>
-    </footer>
-  `;
-  document.body.insertAdjacentHTML("beforeend", footerHTML);
-});
